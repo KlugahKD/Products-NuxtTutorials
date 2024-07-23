08e3198a08763a8f63477d609ea2ce84bfe68166
KlugahKd <klugahdaniel1@gmail.com>
2024-07-23 14:42:01 +0000

    First Commit

---
+++ pages/index.vue
@@ -0,0 +1,34 @@
+<template>
+    <div>
+        <header>
+            <h1>My Nuxt App</h1>
+            <nav>
+                <NuxtLink>My Nuxt App</NuxtLink>
+                <ul>
+                    <li><nuxt-link to="/">Home</nuxt-link></li>
+                    <li><nuxt-link to="/about">About</nuxt-link></li>
+                    <li><nuxt-link to="/products">Products</nuxt-link></li>
+                </ul>
+            </nav>
+
+        </header>
+     <h2>Home</h2>
+     <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sapiente, fugit totam libero hic voluptate porro cupiditate quia quam nam reiciendis unde culpa repudiandae molestias praesentium, nesciunt ipsam temporibus dolor itaque.</p>
+     <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sapiente, fugit totam libero hic voluptate porro cupiditate quia quam nam reiciendis unde culpa repudiandae molestias praesentium, nesciunt ipsam temporibus dolor itaque.</p>
+    </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style scoped>
+h2 {
+    margin-bottom: 20px;
+    font-size: 30px;
+}
+p {
+    font-size: 18px;
+    margin: 20px 0;
+}
+</style>
---
+++ pages/products/[id].vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+        <p> Product details for {{ id }}</p>
+        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Veniam facere labore delectus, odio tempora asperiores repudiandae incidunt ipsam maiores totam sapiente illum amet esse nobis animi odit quod nesciunt similique!</p>
+    
+    </div>
+</template>
+
+<script setup>
+
+const { id } = useRoute().params;
+
+</script>
+
+<style scoped>
+
+</style>
