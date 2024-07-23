189405ea098b60f2c6cd81496e1441f258f69f3e
KlugahKd <klugahdaniel1@gmail.com>
2024-07-23 18:04:11 +0000

    added tailwind

--- pages/about.vue
+++ pages/about.vue
@@ -1,18 +1,5 @@
 <template>
     <div>
-        <header>
-            <h1>My Nuxt App</h1>
-            <nav>
-                <NuxtLink>My Nuxt App</NuxtLink>
-                <ul>
-                    <li><nuxt-link to="/">Home</nuxt-link></li>
-                    <li><nuxt-link to="/about">About</nuxt-link></li>
-                    <li><nuxt-link to="/products">Products</nuxt-link></li>
-                </ul>
-            </nav>
-
-        </header>
-        
      <h2>About</h2>
      <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sapiente, fugit totam libero hic voluptate porro cupiditate quia quam nam reiciendis unde culpa repudiandae molestias praesentium, nesciunt ipsam temporibus dolor itaque.</p>
      <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sapiente, fugit totam libero hic voluptate porro cupiditate quia quam nam reiciendis unde culpa repudiandae molestias praesentium, nesciunt ipsam temporibus dolor itaque.</p>
