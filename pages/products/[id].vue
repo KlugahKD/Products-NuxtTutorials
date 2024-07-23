1f4f8901045e274a8caa4f2025636e168588300c
KlugahKd <klugahdaniel1@gmail.com>
2024-07-23 23:42:55 +0000

    Componenets

--- pages/products/[id].vue
+++ pages/products/[id].vue
@@ -1,14 +1,21 @@
 <template>
     <div>
-        <p> Product details for {{ id }}</p>
-        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Veniam facere labore delectus, odio tempora asperiores repudiandae incidunt ipsam maiores totam sapiente illum amet esse nobis animi odit quod nesciunt similique!</p>
-    
+        <ProductDetails :product="product"/>
     </div>
 </template>
 
 <script setup>
+import ProductDetails from '../components/ProductDetails.vue';
+
 
 const { id } = useRoute().params;
+const uri = `https://fakestoreapi.com/products/${id}`;
+
+const { data : product } = await useFetch(uri);
+
+definePageMeta({
+    layout: 'products',
+})
 
 </script>
 
